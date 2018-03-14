0ce515f919772ce3949772353ce8dadd36b79fd5
Korepanov Michail <hihkore2yandex.ru>
2018-03-14 14:51:33 +0400

    second

---
+++ PHPMailer.php
@@ -0,0 +1,22 @@
+﻿﻿<?php
+$email = ($_POST['email']);
+$phone = ($_POST['phone']);
+$text = ($_POST['text']);
+//strip_tags заменяет знаки < >  на безопасные спецсимволы
+$email = strip_tags($email);
+$phone = strip_tags($phone);
+$text = strip_tags($text);
+
+$headers = "Content-type:text/html ; Charset=utf-8";
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if(mail('admin@landing-sssr.com',"письмо от : " . $email   ,  'телефон :' . $phone . ' | ' . ' текст сообщения : ' . $text , $headers)){
+       echo 'письмо отправвлено';
+    }else{
+       echo 'ошибка';
+    };
+}else{
+   echo "email  указан неверно.";
+};
+?>
+
